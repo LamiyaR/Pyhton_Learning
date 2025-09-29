72fa72dc5f10e39b6a0b26526e5f54e09cc2d4ff
LamiyaR <rampurawallamiya@gmail.com>
2025-09-29 15:34:46 -0700

    done

---
+++ reusable_function.py
@@ -0,0 +1,16 @@
+def square(number):
+    return (number * number)
+
+results = square(3)
+print(results)
+
+
+
+def square(number):
+    print(number * number)
+
+results = square(3)
+print(results)
+
+
+
