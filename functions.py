5708997a60ae5c1b15f1a820b0226e0b82104256
LamiyaR <rampurawallamiya@gmail.com>
2025-09-29 15:19:51 -0700

    done

---
+++ functions.py
@@ -0,0 +1,10 @@
+def great_user(name):
+    print(f"hi there {name}! ")
+    print("welcome")
+
+
+# when ever we are defining a function its good practice to add teo line breaks after it
+print("start")
+great_user("lamiya")
+great_user("lamlam")
+print("finish")
