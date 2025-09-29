719e414f8f8e3c736c2724f33a931d84afb15346
LamiyaR <rampurawallamiya@gmail.com>
2025-09-29 12:32:10 -0700

    nested loops

---
+++ nested_loops.py
@@ -0,0 +1,16 @@
+"""
+
+#NESTED LOOP
+for x in range(4):
+    for y in range(4):
+        print(f'({x}, {y})')
+"""
+
+
+numbers = [2, 2, 2, 2, 8]
+
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
