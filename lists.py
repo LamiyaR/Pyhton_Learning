f93e0b05070c32e9f39d876f86a104a8dcf02e27
LamiyaR <rampurawallamiya@gmail.com>
2025-09-29 13:12:59 -0700

    lists

---
+++ lists.py
@@ -0,0 +1,26 @@
+names = ['lamiya', 'shabbir', 'rampurawala', 'rashida', 'hamza', 'ebrahim']
+
+print (names[0])
+print(names[1])
+print(names[-1])
+print(names[-2])
+print(names[2:])
+print(names[2:4])
+print(names[:])
+print(names)
+names[0] = 'lamiya'
+
+
+
+#write a program to find the largest number in the list
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+max = numbers[0]
+for number in numbers:
+     if number > max:
+         max = number
+print(max)
+
+
+
